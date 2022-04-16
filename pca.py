2c8dfdff442e4610b5aa3e0c841e8d91a8bbdd2c
kausiksubramaniyam <59435311+kausiksubramaniyam@users.noreply.github.com>
2022-04-16 10:32:32 +0530

    Create pca.py

---
+++ pca.py
@@ -0,0 +1,85 @@
+import numpy as np
+
+
+class PCA:
+    def __init__(self, n_components):
+        self.n_components = n_components
+        self.components = None
+        self.mean = None
+
+    def fit(self, X):
+        self.mean = np.mean(X, axis=0)
+        X = X - self.mean
+        cov = np.cov(X.T)
+
+        eigenvalues, eigenvectors = np.linalg.eig(cov)
+
+        eigenvectors = eigenvectors.T
+        idxs = np.argsort(eigenvalues)[::-1]
+        eigenvalues = eigenvalues[idxs]
+        eigenvectors = eigenvectors[idxs]
+
+        self.components = eigenvectors[0 : self.n_components]
+
+    def transform(self, X):
+        X = X - self.mean
+        return np.dot(X, self.components.T)
+def handle_non_numerical_data(df):
+    
+    columns = df.columns.values
+
+    for column in columns:
+        text_digit_vals = {}
+        def convert_to_int(val):
+            return text_digit_vals[val]
+
+        if df[column].dtype != np.int64 and df[column].dtype != np.float64:
+            
+            column_contents = df[column].values.tolist()
+            unique_elements = set(column_contents)
+            x = 0
+            for unique in unique_elements:
+                if unique not in text_digit_vals:
+                    text_digit_vals[unique] = x
+                    x+=1
+            df[column] = list(map(convert_to_int,df[column]))
+
+    return df
+
+if __name__ == "__main__":
+    import matplotlib.pyplot as plt
+    from sklearn import datasets
+
+
+    df = pd.read_csv('output12.csv')
+
+    df.fillna(0,inplace=True)
+
+    df = handle_non_numerical_data(df)
+    print(df.head())
+
+    df.drop(['Patient_DOB'], 1, inplace=True)
+
+
+    X = np.array(df.drop(['Patient_Gender'], 1).astype(float))
+    X = preprocessing.scale(X)
+    y = np.array(df['Patient_Gender'])
+
+    pca = PCA(2)
+    pca.fit(X)
+    X_projected = pca.transform(X)
+
+    print("Shape of X:", X.shape)
+    print("Shape of transformed X:", X_projected.shape)
+
+    x1 = X_projected[:, 0]
+    x2 = X_projected[:, 1]
+
+    plt.scatter(
+        x1, x2, c=y, edgecolor="none", alpha=0.8, cmap=plt.cm.get_cmap("viridis", 3)
+    )
+
+    plt.xlabel("Principal Component 1")
+    plt.ylabel("Principal Component 2")
+    plt.colorbar()
+    plt.show()
